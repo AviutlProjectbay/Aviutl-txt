a0468f10ef0e6efd56ad6dd0b82c5d64cb04670b
AviutlProjectbay <setsuuu1671@gmail.com>
2024-12-10 19:50:30 +0900

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1733827660,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/タイaaaaaaaaaaaaaaaaaトルなし-sheet0.png",
+		"images/pr_aviutl-sheet0.png",
+		"images/screenshot-sheet0.png",
+		"images/385a5ff1c6580eb-sheet0.png",
+		"images/タイトルなし-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
